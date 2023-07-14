bd3c8de6a8be5c1e30c54c2aebae110a3db9df9d
Aarush_009 <aarushkumarchoubey@gmail.com>
2023-07-14 08:24:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Container, Typography, Card, CardContent, Grid, Box } from '@mui/material';
+import axios from 'axios';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.gyanibooks.com/library/get_dummy_notes/')
+      .then((res) => {
+        setNotes(res.data)
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }, []);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Typography variant="h3" gutterBottom>
+        Notes
+      </Typography>
+      <Grid container spacing={2}>
+        {notes.map((note) => {
+          // const innerNotes = JSON.parse(note.notes || "{}");
+
+          const date = note.date_created;
+          const dateObj = new Date(date);
+          const year = dateObj.getFullYear();
+          const month = (dateObj.getMonth() + 1).toString().padStart(2, '0');
+          const day = dateObj.getDate().toString().padStart(2, '0');
+          const hours = dateObj.getHours().toString().padStart(2, '0');
+          const minutes = dateObj.getMinutes().toString().padStart(2, '0');
+          const seconds = dateObj.getSeconds().toString().padStart(2, '0');
+
+          const readableDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+          return (
+            <Grid item xs={12} key={note.id}>
+              <Card>
+                <CardContent>
+                  <Typography variant="h6" gutterBottom>
+                    {note.title.toUpperCase()}
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    ID: {note.id}
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    User: {note.user}
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    UUID: {note.uuid}
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    Category: {note.category}
+                  </Typography>
+                  <Typography variant='subtitle1'>
+                    Notes: {note.notes}
+                  </Typography>
+                  <Typography variant='subtitle2'>
+                    Date Created: {readableDate}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
   );
 }
 
